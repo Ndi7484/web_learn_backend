63382fb5ecb79049e775bf1a7aa387e386a1464b
Ndi7484 <ndimasekaeka10903@gmail.com>
2026-02-21 00:17:30 +0700

    node sum ok

--- middleware.js
+++
@@ -1,7 +0,0 @@
-// middleware.js
-export function middleware(req) {
-  const res = NextResponse.next();
-  res.headers.set("Access-Control-Allow-Origin", "*");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type");
-  return res;
-}
